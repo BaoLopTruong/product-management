6fee034a50c79c5f6c16ad0c5489e112d5342b5a
BaoLopTruong <ngocbaole1999@gmail.com>
2022-06-24 14:13:55 +0700

    update navbar

--- src/components/home/navbar/Navbar.js
+++ src/components/home/navbar/Navbar.js
@@ -1,27 +1,19 @@
 import '../../../main.css'
 import { Link } from 'react-router-dom'
-// import Clock from './Clock'
 
-
-export default function Navbar(props) {
-
+export default function Navbar() {
+      
       return (
             <div >
                   <header className="app-header">
                         {/* <!-- Sidebar toggle button--> */}
                         <span className="app-sidebar__toggle"  data-toggle="sidebar" aria-label="Hide Sidebar"></span>
-                        {/* <!-- Navbar Right Menu--> */}
-                        <ul className="app-nav">
-                              {/* <!-- User Menu--> */}
-                              {/* <li><a className="app-nav__item" href="/index.html"><i className='bx bx-log-out bx-rotate-180'></i> </a>
-                              </li> */}
-                        </ul>
                   </header>
                   {/* <!-- Sidebar menu--> */}
                   <div className="app-sidebar__overlay" data-toggle="sidebar"></div>
                   <aside className="app-sidebar">
                         <div className="app-sidebar__user">
-                              {/* <img className="app-sidebar__user-avatar" src="/images/hay.jpg" width="50px" alt="User Image"> </img> */}
+                              <img className="app-sidebar__user-avatar" src={'../../../img-staff/lnb.jpg'} width="50px" alt="User Imagee"> </img>
                               <div>
                                     <p className="app-sidebar__user-name"><b>Lê Ngọc Bảo</b></p>
                                     <p className="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
@@ -47,10 +39,6 @@ export default function Navbar(props) {
                                           <span className="app-menu__label">Quản lý nhân viên</span>
                                     </Link>
                               </li>
-                              {/* <li><a className="app-menu__item" href="#">
-                                    <i className='app-menu__icon bx bx-user-voice'></i>
-                                    <span className="app-menu__label">Quản lý khách hàng</span></a>
-                              </li> */}
                               <li>
                                     <Link to={'/product'} className="app-menu__item" >
                                           <i className='app-menu__icon bx bx-purchase-tag-alt'></i>
@@ -63,21 +51,6 @@ export default function Navbar(props) {
                                           <span className="app-menu__label">Quản lý đơn hàng</span>
                                     </Link>
                               </li>
-                              {/* <li>
-                                    <Link to={'/internal'} className="app-menu__item" >
-                                          <i className='app-menu__icon bx bx-run'></i>
-                                          <span
-                                                className="app-menu__label">Quản lý nội bộ
-                                          </span>
-                                    </Link>
-                              </li> */}
-                              {/* <li>
-                                    <Link to={'/tablemoney'} className="app-menu__item" >
-                                          <i className='app-menu__icon bx bx-dollar'></i>
-                                          <span
-                                                className="app-menu__label">Bảng kê lương</span>
-                                    </Link>
-                              </li> */}
                               <li>
                                     <Link to={'/report'} className="app-menu__item" >
                                           <i className='app-menu__icon bx bx-pie-chart-alt-2'></i>
@@ -93,22 +66,12 @@ export default function Navbar(props) {
                               <li>
                                     <Link to={'/setting'} className="app-menu__item" >
                                           <i className='app-menu__icon bx bx-cog'></i>
-                                          <span className="app-menu__label">Cài đặt hệ thống</span>
+                                          <span className="app-menu__label">Cài đặt</span>
                                     </Link>
                               </li>
                         </ul>
                   </aside>
-                  <div className="row">
-                <div className="col-md-12">
-                    <div className="app-title">
-                        <ul className="app-breadcrumb breadcrumb">
-                            <li className="breadcrumb-item"><b> {props.location}</b></li>
-                        </ul>
-                        {/* <div id="clock" ><span className='date'> <p style={{float: 'right'}}>{hours}:{mintues}:{seconds} </p> <br></br>   
-                        <p style={{ float: 'right'}}> {day}, ngày {date}, tháng {month}, năm {year}</p></span>  </div> */}
-                    </div>
-                </div>
-            </div>
+    
             </div>
       )
 }
